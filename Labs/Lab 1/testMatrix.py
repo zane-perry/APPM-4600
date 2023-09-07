8e6770351a2c38421418cc23ff87fbf18a68c983
Zane Perry <Zane.Perry@colorado.edu>
2023-09-07 14:47:30 -0600

    Added comment to Lab 1 code

--- Labs/Lab 1/testMatrix.py
+++ Labs/Lab 1/testMatrix.py
@@ -7,9 +7,11 @@ def driver():
     m = 2
     n = 2
 
+    #initializing the vector and Matrix that will be used for the program
     A = np.array([[1,2],[3,4]])
     x = np.array([5,6])
 
+    #Calling the helper function
     product = matrixByVector(A,x,m,n)
 
     print('the product is ', product)
@@ -29,7 +31,9 @@ def matrixByVector(A,x,m,n):
     result = np.zeros(m)
     
     for i in range(m):
+        #Calling dotProduct helper function
         result[i] = dotProduct(A[i],x,n)
+        #for every row in A, compute the dot product of that row and the vector x
 
     return result
 
@@ -39,6 +43,7 @@ def matrixByVector(A,x,m,n):
 
 def dotProduct(x,y,n):
 
+    #Dot product code taken from the other file
     dp = 0.
     for j in range(n):
        dp = dp + x[j]*y[j]
@@ -46,4 +51,5 @@ def dotProduct(x,y,n):
     return dp
 
 
+#Final call to the wrapper function to run the entire code
 driver()
